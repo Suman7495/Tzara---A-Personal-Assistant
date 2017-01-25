692640ee4b2ea0779961aec8e84c3fc1f06aa8b8
Suman7495 <suman7495@gmail.com>
2017-01-25 22:44:42 +0530

    Update Speech.py

--- Speech.py
+++ Speech.py
@@ -9,19 +9,16 @@ def speak(out_string):
     Speaks using gTTs from the internet.
     """
     os.system("if ! ping -c 1 www.google.com>/dev/null;"
-        "then echo fail >/media/suman/New\ Volume1/Artificial\ Intelligence"
-        "/Personal\ Assistant/Personal\ Assistant/Text_files/ping_result.txt;"
-        "else echo success>/media/suman/New\ Volume1/Artificial\ Intelligence"
-        "/Personal\ Assistant/Personal\ Assistant/Text_files/ping_result.txt;"
+        "then echo fail >/path/to/the/file/ping_result.txt;"    #modify
+        "else echo success>/path/to/the/file/ping_result.txt;"  #modify
         "fi;")
-    f_ping = open("/media/suman/New Volume1/Artificial Intelligence"
-        "/Personal Assistant/Personal Assistant/Text_files/ping_result.txt")
+    f_ping = open("/path/to/the/file/ping_result.txt")  #modify
     result = f_ping.read().strip().split("\n")
     f_ping.close()
     print '\033[1m' + 'Tzara: ' + '\033[0m' + out_string
-    #if result[0] == 'success':
-     #   tts = gTTS(text = out_string, lang = 'en')
-      #  tts.save("hello.mp3")
-       # os.system("mpg321 -q hello.mp3")
+    if result[0] == 'success':
+        tts = gTTS(text = out_string, lang = 'en')
+        tts.save("hello.mp3")
+        os.system("mpg321 -q hello.mp3")
 
 
