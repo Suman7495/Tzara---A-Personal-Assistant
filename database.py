6283f3305e1de87aab74be305678dc5034cec8fb
Suman7495 <suman7495@gmail.com>
2017-01-25 21:41:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,18 @@
+"""
+Creates database to generate sentences.
+
+Modify:
+8: f_corpora = open("/path/to/the/file/corpora.txt","r")
+12: f = open("/path/to/the/file/nltk_data/corpora/gutenberg/" + name.strip())
+"""
 def database():
-	f_corpora=open("/media/suman/New Volume1/Artificial Intelligence/Personal Assistant/Personal Assistant/Text_files/corpora.txt","r")
-	data=[]
+	f_corpora = open("/path/to/the/file/corpora.txt","r")     #modify
+	data = []
 	for name in f_corpora:
-		f=open("/home/suman/nltk_data/corpora/gutenberg/"+ name.strip())
+		f = open("/path/to/the/file/nltk_data/corpora/gutenberg/" + name.strip())    #modify
 		f.seek(0)
-		data_temp=f.read()
-		data=data+data_temp.split()
+		data_temp = f.read()
+		data = data + data_temp.split()
 		del data_temp
 		
 	f_corpora.close()
