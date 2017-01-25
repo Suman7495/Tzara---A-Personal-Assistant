662802fe4ca690d814c8e863ab815124efc06389
Suman7495 <suman7495@gmail.com>
2017-01-25 21:45:54 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,5 +1,8 @@
 """
 Opens folders
+
+Modify
+19: f_fldr = open("/path/to/the/file/folder.txt", "r")    #modify
 """
 
 import os
@@ -13,16 +16,15 @@ def open_folder(name):
     name = name.strip()
     #os.system("find -type d -iname '*"+name +"*' -exec xdg-open ")
     counter = 0
-    f_fold = open("/media/suman/New Volume1/Artificial Intelligence"
-        "/Personal Assistant/Personal Assistant/Text_files/folder.txt", "r")
-    for line in f_fold:
+    f_fldr = open("/path/to/the/file/folder.txt", "r")    #modify
+    for line in f_fldr:
         name_list = line.strip().split("-")
         if name.lower() == ''.join(name_list[0:1]).lower():
             counter = 1
             path = ''.join(name_list[1:2])
             os.system('xdg-open ' + path)
-            speak("Opened folder.")
+            speak("Opened the requested folder.")
             break
-    f_fold.close()
+    f_fldr.close()
     if counter == 0:
-        speak("Sorry could not find folder.")
+        speak("Sorry, but I could not find the folder.")
