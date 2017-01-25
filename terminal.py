2fb9c64e710cfd1eacd15d8d5a08ccb41d819f07
Suman7495 <suman7495@gmail.com>
2017-01-25 22:45:25 +0530

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -11,7 +11,7 @@ def terminal():
     speak("You are into the terminal now."
         "Type 'exit' when you want to quit.")
     while True:
-        command = raw_input('\033[1m' + 'Suman: ' + '\033[0m')
+        command = raw_input('\033[1m' + 'Username: ' + '\033[0m')   #modify
         if command.lower() == "exit":
             speak("Quit terminal.")
             return
