1a1535af7ab7841f1edc6981a73ea31aa4de5ee4
Suman Pal <suman7495@gmail.com>
2017-01-28 18:17:41 +0530

    Update Speech.py

--- Speech.py
+++ Speech.py
@@ -8,12 +8,12 @@ def speak(out_string):
     """
     Speaks using gTTs from the internet.
     """
-    path = os.getcwd() + "
+    path = os.getcwd() + "/Text_Files/ping_result.txt"
     os.system("if ! ping -c 1 www.google.com>/dev/null;"
-        "then echo fail >/path/to/the/file/ping_result.txt;"    #modify
-        "else echo success>/path/to/the/file/ping_result.txt;"  #modify
+        "then echo fail >" path+";"    #modify
+        "else echo success>" path";"  #modify
         "fi;")
-    f_ping = open("/path/to/the/file/ping_result.txt")  #modify
+    f_ping = open(path)  #modify
     result = f_ping.read().strip().split("\n")
     f_ping.close()
     print '\033[1m' + 'Tzara: ' + '\033[0m' + out_string
