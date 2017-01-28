3099fee33851f8fa7e2e009b873aaf44540415ff
Suman Pal <suman7495@gmail.com>
2017-01-28 18:26:26 +0530

    Update confirm.py

--- confirm.py
+++ confirm.py
@@ -2,13 +2,15 @@
 Gets confirmation.
 
 """
+import os
 def confirm(reply):
     """
     Checks if reply is in confirmation list.
     If yes, success.
     Else, failure.
     """
-    f_ok = open("path/to/file/confirmations.txt", "r")    #modify
+    path = os.getcwd() + "Text_Files/confirmations.txt"
+    f_ok = open(path, "r")    
     confirmation_list = f_ok.read().strip().split()
     f_ok.close()
     return reply.lower() in confirmation_list
