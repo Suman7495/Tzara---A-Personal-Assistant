91fbb897851a0c1988c3f7b03a7fd80290c47f7b
Suman Pal <suman7495@gmail.com>
2017-01-31 12:58:06 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tzara',
+      version='0.1',
+      description='A Virtual Personal Assistant',
+      url='https://github.com/Suman7495/Tzara---A-Personal-Assistant',
+      author='Suman Pal',
+      author_email='suman7495@gmail.com',
+      license='MIT',
+      packages=['tzara'],)
