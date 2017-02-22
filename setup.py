34e6adac1c6f22d239e80559cd6bb2c3d41c1787
Suman Pal <suman7495@gmail.com>
2017-02-22 11:14:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ class MyInstall(install):
     def run(self):
         install.run(self)
         path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
-        os.system("chmod +x "+path+"startup.sh")
-        os.system("sh "+path+"startup.sh")
         os.system("echo 'Setting up Tzara.desktop'")
         os.system("chmod +x "+path+"desktopsetup.sh")
         os.system("bash "+path+"desktopsetup.sh")
+        os.system("chmod +x "+path+"startup.sh")
+        os.system("sh "+path+"startup.sh")
+        
 
 setup(name='Tzara---A-Personal-Assistant',
-      version='1.0.3',
+      version='1.0.4',
       description='A Virtual Personal Assistant',
       url='https://github.com/Suman7495/Tzara---A-Personal-Assistant',
       author='Suman Pal',
