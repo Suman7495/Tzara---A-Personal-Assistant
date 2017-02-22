cadf1c69da26c4d87ed77fd7baa0192f473b425b
Suman Pal <suman7495@gmail.com>
2017-02-22 18:41:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ class MyInstall(install):
         
 
 setup(name='Tzara---A-Personal-Assistant',
-      version='1.1.4',
+      version='1.1.5',
       description='A Virtual Personal Assistant',
       url='https://github.com/Suman7495/Tzara---A-Personal-Assistant',
       author='Suman Pal',
