37bc96ab7341115611b388ad13180fd73321f948
Suman Pal <suman7495@gmail.com>
2017-02-22 14:55:05 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -32,6 +32,8 @@ sudo apt-get install guake
 echo "Installing xdg-utils..."
 sudo apt-get install xdg-utils
 
+echo "Setting up .desktop file"
+
 STR=$'[Desktop Entry]
 Encoding=UTF-8
 Type=Application
@@ -40,7 +42,7 @@ Comment=A Virtual Personal Assistant
 Exec=guake -n guake -t -e "python /usr/local/lib/python2.7/dist-packages/tzara/main.pyc" guake -r "Tzara"
 Terminal=True
 Type=Application
-Categories=Application'
+Categories=Application;'
 if [ ! -e /usr/share/applications/Tzara.desktop  ]; then
   echo "Setting up .desktop file"
   echo "$STR" | sudo tee sudo /usr/share/applications/Tzara.desktop > /dev/null
